048ed890a5f282df11dc891bd943be4e8fae2fa9
homerioni <homerion13@gmail.com>
2022-08-17 11:45:09 +0300

    create header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 'use strict'
 
 $(document).ready(function () {
+
+    // Burger menu
+    $('.header__burger-btn').click(function () {
+        $('body').addClass('lock');
+        $('.header__burger-menu').addClass('active');
+    });
+    $('.header__burger-close').click(function () {
+        $('body').removeClass('lock');
+        $('.header__burger-menu').removeClass('active');
+    });
+
 });
